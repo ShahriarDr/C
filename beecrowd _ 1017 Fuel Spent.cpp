02aaa6e0b6014d516c3f5d49bb9c27a36ad90df3
Shahriar Shimul <shimulmd814@gmail.com>
2025-09-01 09:10:46 +0600

    Add files via upload

---
+++ beecrowd _ 1017 Fuel Spent.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{int t,d;
+float f;
+scanf("%d %d",&t,&d);
+f=(t*d)/12;
+printf("%.3f\n",f);
+    return 0;
+}
---
+++ beecrowd _ 1018 Banknotes.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{int N,r;
+scanf("%d",&N);
+printf("%d\n",N);
+r=N/100;
+printf("%d nota(s) de R$ 100,00\n",r);
+r=N%100;
+r=r/50;
+printf("%d nota(s) de R$ 50,00\n",r);
+r=N%100;
+r=r%50;
+r=r/20;
+printf("%d nota(s) de R$ 20,00\n",r);
+r=N%100;
+r=r%50;
+r=r%20;
+r=r/10;
+printf("%d nota(s) de R$ 10,00\n",r);
+r=N%100;
+r=r%50;
+r=r%20;
+r=r%10;
+r=r/5;
+printf("%d nota(s) de R$ 5,00\n",r);
+r=N%100;
+r=r%50;
+r=r%20;
+r=r%10;
+r=r%5;
+r=r/2;
+printf("%d nota(s) de R$ 2,00\n",r);
+r=N%100;
+r=r%50;
+r=r%20;
+r=r%10;
+r=r%5;
+r=r%2;
+r=r/1;
+printf("%d nota(s) de R$ 1,00\n",r);
+    return 0;
+}
